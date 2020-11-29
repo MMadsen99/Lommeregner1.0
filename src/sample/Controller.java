69d95375da8b798704e30cf0262b5b96f326ad62
MathiasMadsen <m1999@live.dk>
2020-11-29 09:27:58 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,62 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+
+public class Controller {
+
+    @FXML
+    TextField Text1;
+    @FXML
+    TextField Text2;
+    @FXML
+    TextField TextResult;
+
+
+    public void plus(){
+
+        int første = Integer.parseInt(Text1.getText());
+
+        int anden = Integer.parseInt(Text2.getText());
+
+        int result = første + anden;
+
+        TextResult.setText(String.valueOf(result));
+
+    }
+    public void minus(){
+
+        int første = Integer.parseInt(Text1.getText());
+
+        int anden = Integer.parseInt(Text2.getText());
+
+        int result = første - anden;
+
+        TextResult.setText(String.valueOf(result));
+
+    }
+    public void gange(){
+
+        int første = Integer.parseInt(Text1.getText());
+
+        int anden = Integer.parseInt(Text2.getText());
+
+        int result = første * anden;
+
+        TextResult.setText(String.valueOf(result));
+
+    }
+    public void dividere(){
+
+        int første = Integer.parseInt(Text1.getText());
+
+        int anden = Integer.parseInt(Text2.getText());
+
+        int result = første / anden;
+
+        TextResult.setText(String.valueOf(result));
+
+    }
+}
